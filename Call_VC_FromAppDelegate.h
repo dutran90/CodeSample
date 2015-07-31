b23f5eeec5b96c8b37ba11025a25d0a036247803
Alex Tran <alextran@Alexs-MacBook-Air.local>
2015-07-31 08:33:11 +0700

    /SVN/CodeSample/Call_VC_FromAppDelegate.h

--- Call_VC_FromAppDelegate.h
+++ Call_VC_FromAppDelegate.h
@@ -1,20 +1,7 @@
-//
-//  Call_VC_FromAppDelegate.h
-//  LottoFY
-//
-//  Created by Alex Tran on 7/31/15.
-//  Copyright (c) 2015 TruongBKDN. All rights reserved.
-//
-
-#ifndef LottoFY_Call_VC_FromAppDelegate_h
-#define LottoFY_Call_VC_FromAppDelegate_h
-
+#AlexTran
 
 NewVC *vcNew = [[NewVC alloc] initWithNibName:@"NewVC" bundle:nil];
 UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:vcNew];
 [self.window setRootViewController:nav];
 [nav setNavigationBarHidden:YES];
 
-
-
-#endif
